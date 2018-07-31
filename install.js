76c3e34246c34e67bde5b34a10c1da502756e9eb
Adam Bouhenguel <adam@bouhenguel.com>
2018-07-30 18:53:58 -0700

    Update Electron 1.6.11 -> 1.8.7

--- install.js
+++ install.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 var path = require("path");
 var spawn = require("child_process").spawn;
 var demokitDirectory = path.join(process.cwd(), "electron");
-var installFlags = ["install", "--local", "--runtime=electron", "--target=1.6.11", "--disturl=https://atom.io/download/atom-shell", "--abi=53"];
+var installFlags = ["install", "--local", "--runtime=electron", "--target=1.8.7", "--disturl=https://atom.io/download/atom-shell", "--abi=53"];
 
 spawn("npm", installFlags,
 {
